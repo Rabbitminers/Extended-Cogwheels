e3476a7ddb67bef8d18a3a0dd5ae06613f0d0564
Charlie Wood <rabbitminers2.0@gmail.com>
2022-12-16 15:53:35 +0000

    Ported to 0.5.0e

--- src/main/java/com/rabbitminers/extendedgears/index/AddonTileEntities.java
+++ src/main/java/com/rabbitminers/extendedgears/index/AddonTileEntities.java
@@ -12,7 +12,7 @@ public class AddonTileEntities {
     public static final BlockEntityEntry<CogWheelKineticTileEntity> BRACKETED_KINETIC = REGISTRATE
             .tileEntity("cogwheeltileentity", CogWheelKineticTileEntity::new)
             .instance(() -> CogWheelKineticTileInstance::new, false)
-            .validBlocks(AddonBlocks.COGWHEEL, AddonBlocks.LARGE_COGWHEEL, AddonBlocks.BIRCH_COGWHEEL, AddonBlocks.LARGE_BIRCH_COGWHEEL, AddonBlocks.OAK_COGWHEEL, AddonBlocks.LARGE_OAK_COGWHEEL, AddonBlocks.DARK_OAK_COGWHEEL, AddonBlocks.LARGE_DARK_OAK_COGWHEEL, AddonBlocks.ACACIA_COGWHEEL, AddonBlocks.LARGE_ACACIA_COGWHEEL, AddonBlocks.JUNGLE_COGWHEEL, AddonBlocks.LARGE_JUNGLE_COGWHEEL, AddonBlocks.WARPED_COGWHEEL, AddonBlocks.LARGE_WARPED_COGWHEEL, AddonBlocks.CRIMSON_COGWHEEL, AddonBlocks.LARGE_CRIMSON_COGWHEEL, AddonBlocks.LARGE_STEEL_COGWHEEL, AddonBlocks.STEEL_COGWHEEL, AddonBlocks.COPPER_COGWHEEL, AddonBlocks.LARGE_COPPER_COGWHEEL, AddonBlocks.IRON_COGWHEEL, AddonBlocks.LARGE_IRON_COGWHEEL)
+            .validBlocks(AddonBlocks.BIRCH_COGWHEEL, AddonBlocks.LARGE_BIRCH_COGWHEEL, AddonBlocks.OAK_COGWHEEL, AddonBlocks.LARGE_OAK_COGWHEEL, AddonBlocks.DARK_OAK_COGWHEEL, AddonBlocks.LARGE_DARK_OAK_COGWHEEL, AddonBlocks.ACACIA_COGWHEEL, AddonBlocks.LARGE_ACACIA_COGWHEEL, AddonBlocks.JUNGLE_COGWHEEL, AddonBlocks.LARGE_JUNGLE_COGWHEEL, AddonBlocks.WARPED_COGWHEEL, AddonBlocks.LARGE_WARPED_COGWHEEL, AddonBlocks.CRIMSON_COGWHEEL, AddonBlocks.LARGE_CRIMSON_COGWHEEL, AddonBlocks.LARGE_STEEL_COGWHEEL, AddonBlocks.STEEL_COGWHEEL, AddonBlocks.COPPER_COGWHEEL, AddonBlocks.LARGE_COPPER_COGWHEEL, AddonBlocks.IRON_COGWHEEL, AddonBlocks.LARGE_IRON_COGWHEEL)
             .renderer(() -> CogWheelKineticTileRenderer::new)
             .register();
     public static void register() {}
