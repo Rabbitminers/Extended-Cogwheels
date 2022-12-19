bcdab368153372dd376c86fe11da4496efcd902e
Charlie Wood <rabbitminers2.0@gmail.com>
2022-12-19 19:50:04 +0000

    Fixed encasing cogs reverting them to default cogwheels

--- src/main/java/com/rabbitminers/extendedgears/basecog/MetalCogWheel.java
+++ src/main/java/com/rabbitminers/extendedgears/basecog/MetalCogWheel.java
@@ -8,10 +8,15 @@ import com.simibubi.create.content.contraptions.base.KineticTileEntity;
 import com.simibubi.create.content.contraptions.relays.elementary.CogWheelBlock;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -47,5 +52,10 @@ public class MetalCogWheel extends CogWheelBlock implements ICustomCogWheel {
     @Override
     public BlockEntityType<? extends KineticTileEntity> getTileEntityType() {
         return AddonTileEntities.BRACKETED_KINETIC.get();
+    }
+    @Override
+    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult ray) {
+        // TODO: Allow for flywheel types to be encased
+        return InteractionResult.PASS;
     }
 }
