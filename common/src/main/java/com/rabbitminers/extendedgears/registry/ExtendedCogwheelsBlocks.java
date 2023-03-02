a17f8bf0a84ae1d361d0e5e7d5100d6d9719f3c5
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-02 21:40:08 +0000

    Added CogwheelBlocks

--- common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsBlocks.java
+++ common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsBlocks.java
@@ -1,14 +1,59 @@
 package com.rabbitminers.extendedgears.registry;
 
 import com.rabbitminers.extendedgears.ExtendedCogwheels;
+import com.rabbitminers.extendedgears.base.datatypes.MetalBlockList;
+import com.rabbitminers.extendedgears.base.datatypes.WoodenBlockList;
+import com.rabbitminers.extendedgears.cogwheels.CustomCogwheelBlock;
+import com.simibubi.create.content.contraptions.relays.elementary.BracketedKineticBlockModel;
+import com.simibubi.create.content.contraptions.relays.elementary.CogwheelBlockItem;
+import com.simibubi.create.foundation.block.BlockStressDefaults;
+import com.simibubi.create.foundation.data.BlockStateGen;
 import com.simibubi.create.foundation.data.CreateRegistrate;
-import com.tterrag.registrate.util.entry.BlockEntry;
-import net.minecraft.world.level.block.Block;
+import com.simibubi.create.foundation.data.SharedProperties;
+import com.simibubi.create.foundation.data.TagGen;
+import com.tterrag.registrate.builders.BlockBuilder;
+import com.tterrag.registrate.util.nullness.NonNullUnaryOperator;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.MaterialColor;
 
 public class ExtendedCogwheelsBlocks {
 	public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(ExtendedCogwheels.MOD_ID);
 
-	public static final BlockEntry<Block> EXAMPLE_BLOCK = REGISTRATE.block("example_block", Block::new).register();
+	public static <B extends CustomCogwheelBlock, P> NonNullUnaryOperator<BlockBuilder<B, P>> commonCogwheelTransformer() {
+		return b -> b.blockstate(BlockStateGen.axisBlockProvider(false))
+				.onRegister(CreateRegistrate.blockModel(() -> BracketedKineticBlockModel::new))
+				.transform(BlockStressDefaults.setNoImpact())
+				.item(CogwheelBlockItem::new)
+				.build();
+	}
+
+	public static <B extends CustomCogwheelBlock, P> NonNullUnaryOperator<BlockBuilder<B, P>> woodenCogwheelTransformer() {
+		return b -> b.initialProperties(SharedProperties::wooden)
+				.properties(p -> p.sound(SoundType.WOOD))
+				.properties(p -> p.color(MaterialColor.DIRT))
+				.transform(TagGen.axeOrPickaxe())
+				.transform(commonCogwheelTransformer());
+	}
+
+	public static <B extends CustomCogwheelBlock, P> NonNullUnaryOperator<BlockBuilder<B, P>> metalCogwheelTransformer() {
+		return b -> b.initialProperties(SharedProperties::softMetal)
+				.properties(p -> p.sound(SoundType.METAL))
+				.properties(p -> p.color(MaterialColor.METAL))
+				.transform(TagGen.pickaxeOnly())
+				.transform(commonCogwheelTransformer());
+	}
+
+	public static final WoodenBlockList<CustomCogwheelBlock> WOODEN_COGWHEELS = new WoodenBlockList<>(wood ->
+			REGISTRATE.block(wood.asId() + "_cogwheel", p -> CustomCogwheelBlock.small(p,
+					ExtendedCogwheelsPartials.WOODEN_COGWHEELS.get(wood)))
+							.transform(woodenCogwheelTransformer()).register()
+	);
+
+	public static final MetalBlockList<CustomCogwheelBlock> METAL_COGWHEELS = new MetalBlockList<>(metal ->
+			REGISTRATE.block(metal.asId() + "_cogwheel", p -> CustomCogwheelBlock.small(p,
+					ExtendedCogwheelsPartials.METAL_COGWHEELS.get(metal)))
+							.transform(metalCogwheelTransformer()).register()
+	);
 
 	public static void init() {
 		ExtendedCogwheels.LOGGER.info("Registering blocks for " + ExtendedCogwheels.NAME);
