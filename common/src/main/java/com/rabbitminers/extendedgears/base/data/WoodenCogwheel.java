b20f8683a4d73baf211170c9bf11e33b09590d0d
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-05 22:25:28 +0000

    Implemented ICogwheelMaterial To Wooden Cogwheel

--- common/src/main/java/com/rabbitminers/extendedgears/base/data/WoodenCogwheel.java
+++ common/src/main/java/com/rabbitminers/extendedgears/base/data/WoodenCogwheel.java
@@ -2,11 +2,15 @@ package com.rabbitminers.extendedgears.base.data;
 
 import com.rabbitminers.extendedgears.base.datatypes.IngredientProvider;
 import com.rabbitminers.extendedgears.base.datatypes.IngredientProvider.Namespace;
+import com.rabbitminers.extendedgears.cogwheels.CustomCogwheelBlock;
+import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsBlocks;
+import com.tterrag.registrate.util.entry.BlockEntry;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.NotNull;
 
 
-public enum WoodenCogwheel {
+public enum WoodenCogwheel implements ICogwheelMaterial {
     DARK_OAK(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.DARK_OAK_PLANKS))),
     OAK(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.OAK_PLANKS))),
     BIRCH(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.BIRCH_PLANKS))),
@@ -22,7 +26,24 @@ public enum WoodenCogwheel {
         this.ingredients = ingredients;
     }
 
-    public String asId() {
+    @Override
+    public BlockEntry<CustomCogwheelBlock> getLargeCogwheel() {
+        return ExtendedCogwheelsBlocks.LARGE_WOODEN_COGWHEELS.get(this);
+    }
+
+    @Override
+    public BlockEntry<CustomCogwheelBlock> getSmallCogwheel() {
+        return ExtendedCogwheelsBlocks.WOODEN_COGWHEELS.get(this);
+    }
+
+
+    @Override
+    public @NotNull String asId() {
         return name().toLowerCase();
+    }
+
+    @Override
+    public IngredientProvider[] getIngredients() {
+        return this.ingredients;
     }
 }
