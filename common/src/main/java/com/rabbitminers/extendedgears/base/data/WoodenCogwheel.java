e86e9a6211ab395ffbd0fb5835582ddd77eb0d70
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-05 20:39:36 +0000

    Implemented IngredientProvider to WoodenCogwheel

--- common/src/main/java/com/rabbitminers/extendedgears/base/data/WoodenCogwheel.java
+++ common/src/main/java/com/rabbitminers/extendedgears/base/data/WoodenCogwheel.java
@@ -1,11 +1,26 @@
 package com.rabbitminers.extendedgears.base.data;
 
-import com.jozufozu.flywheel.core.PartialModel;
-import org.jetbrains.annotations.Nullable;
+import com.rabbitminers.extendedgears.base.datatypes.IngredientProvider;
+import com.rabbitminers.extendedgears.base.datatypes.IngredientProvider.Namespace;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+
 
 public enum WoodenCogwheel {
-    DARK_OAK, OAK, BIRCH, JUNGLE, ACACIA, WARPED, CRIMSON
+    DARK_OAK(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.DARK_OAK_PLANKS))),
+    OAK(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.OAK_PLANKS))),
+    BIRCH(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.BIRCH_PLANKS))),
+    JUNGLE(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.JUNGLE_PLANKS))),
+    ACACIA(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.ACACIA_PLANKS))),
+    WARPED(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.WARPED_PLANKS))),
+    CRIMSON(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.CRIMSON_PLANKS)))
     ;
+
+    public final IngredientProvider[] ingredients;
+
+    WoodenCogwheel(IngredientProvider... ingredients) {
+        this.ingredients = ingredients;
+    }
 
     public String asId() {
         return name().toLowerCase();
