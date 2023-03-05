ea8f78190a918b69e5485a40877c837b49fb35bc
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-05 22:25:40 +0000

    Re-ran datagen

--- common/src/main/java/com/rabbitminers/extendedgears/base/datatypes/IngredientProvider.java
+++ common/src/main/java/com/rabbitminers/extendedgears/base/datatypes/IngredientProvider.java
@@ -5,8 +5,8 @@ import net.minecraft.world.item.crafting.Ingredient;
 import java.util.Locale;
 
 public class IngredientProvider {
-    private final Namespace namespace;
-    private final Ingredient ingredient;
+    public final Namespace namespace;
+    public final Ingredient ingredient;
 
     public IngredientProvider(Namespace namespace, Ingredient ingredient) {
         this.namespace = namespace;
