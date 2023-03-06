b19a460ccc8664234165ca5b081fad66fbaa580a
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-06 18:57:39 +0000

    Implemented Util Methods

--- common/src/main/java/com/rabbitminers/extendedgears/base/data/ICogwheelMaterial.java
+++ common/src/main/java/com/rabbitminers/extendedgears/base/data/ICogwheelMaterial.java
@@ -3,6 +3,8 @@ package com.rabbitminers.extendedgears.base.data;
 import com.rabbitminers.extendedgears.base.datatypes.IngredientProvider;
 import com.rabbitminers.extendedgears.cogwheels.CustomCogwheelBlock;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,6 +19,11 @@ public interface ICogwheelMaterial {
 
     @Nullable
     public BlockEntry<CustomCogwheelBlock> getSmallCogwheel();
+
+    @Nullable
+    default TagKey<Item>[] getRecipeTags() {
+        return null;
+    }
 
     public @NotNull String asId();
 
--- common/src/main/java/com/rabbitminers/extendedgears/base/data/MetalCogwheel.java
+++ common/src/main/java/com/rabbitminers/extendedgears/base/data/MetalCogwheel.java
@@ -4,12 +4,15 @@ import com.rabbitminers.extendedgears.base.datatypes.IngredientProvider;
 import com.rabbitminers.extendedgears.base.datatypes.IngredientProvider.Namespace;
 import com.rabbitminers.extendedgears.cogwheels.CustomCogwheelBlock;
 import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsBlocks;
+import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsTags;
+import com.simibubi.create.AllItems;
 import com.simibubi.create.AllTags;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import org.jetbrains.annotations.NotNull;
@@ -17,14 +20,23 @@ import org.jetbrains.annotations.Nullable;
 
 public enum MetalCogwheel implements ICogwheelMaterial {
     IRON(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.IRON_INGOT))),
-    STEEL(new IngredientProvider(Namespace.FORGE, Ingredient.of(AllTags.forgeItemTag("ingots/steel"))),
-        new IngredientProvider(Namespace.FABRIC, Ingredient.of(AllTags.optionalTag(Registry.ITEM, new ResourceLocation("c", "steel_ingots"))))),
+    STEEL(new TagKey[] {ExtendedCogwheelsTags.forgeSteel, ExtendedCogwheelsTags.fabricSteel},
+            new IngredientProvider(Namespace.FORGE, Ingredient.of(ExtendedCogwheelsTags.forgeSteel)),
+            new IngredientProvider(Namespace.FABRIC, Ingredient.of(ExtendedCogwheelsTags.fabricSteel))),
     COPPER(new IngredientProvider(Namespace.COMMON, Ingredient.of(Items.COPPER_INGOT)));
 
     public final IngredientProvider[] ingredients;
+    @Nullable
+    public final TagKey<Item>[] tagKey;
 
     MetalCogwheel(IngredientProvider... ingredients) {
         this.ingredients = ingredients;
+        this.tagKey = null;
+    }
+
+    MetalCogwheel(TagKey<Item>[] tagKey, IngredientProvider... ingredients) {
+        this.ingredients = ingredients;
+        this.tagKey = tagKey;
     }
 
     @Override
@@ -35,6 +47,11 @@ public enum MetalCogwheel implements ICogwheelMaterial {
     @Override
     public @Nullable BlockEntry<CustomCogwheelBlock> getSmallCogwheel() {
         return ExtendedCogwheelsBlocks.METAL_COGWHEELS.get(this);
+    }
+
+    @Override
+    public @Nullable TagKey<Item>[] getRecipeTags() {
+        return this.tagKey;
     }
 
     @Override
