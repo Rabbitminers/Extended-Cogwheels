e9a72cad9c4aed4cbf0887cf0140798e16356c56
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-07 16:56:20 +0000

    Added Shaft Model Specification to ICustomCogwheel

--- common/src/main/java/com/rabbitminers/extendedgears/cogwheels/ICustomCogwheel.java
+++ common/src/main/java/com/rabbitminers/extendedgears/cogwheels/ICustomCogwheel.java
@@ -4,8 +4,9 @@ import com.jozufozu.flywheel.core.PartialModel;
 import com.simibubi.create.AllBlockPartials;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 
-public interface ICustomCogwheel {
+public interface ICustomCogwheel{
     static PartialModel getPartialModelType(BlockState state) {
         return getPartialModelType(state.getBlock());
     }
@@ -14,6 +15,22 @@ public interface ICustomCogwheel {
         if (!(block instanceof ICustomCogwheel cogWheel))
             return null;
         return cogWheel.getPartialModelType();
+    }
+
+    static PartialModel getShaftPartialModelType(BlockState state) {
+        return getShaftPartialModelType(state);
+    }
+
+    static PartialModel getShaftPartialModelType(Block block) {
+        if (!(block instanceof ICustomCogwheel cogWheel))
+            return null;
+        return cogWheel.getShaftPartialModel();
+    }
+
+
+    @Nullable
+    default PartialModel getShaftPartialModel() {
+        return AllBlockPartials.COGWHEEL_SHAFT;
     }
 
     default PartialModel getPartialModelType() {
