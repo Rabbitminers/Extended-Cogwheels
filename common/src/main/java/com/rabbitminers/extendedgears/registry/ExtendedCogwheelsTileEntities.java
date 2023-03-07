7955d81932bb0880f6eedfc7b085f89c6a94acab
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-07 16:57:13 +0000

    Registered Shaftless Cogwheel Tile Entity

--- common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsTileEntities.java
+++ common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsTileEntities.java
@@ -4,6 +4,7 @@ import com.rabbitminers.extendedgears.ExtendedCogwheels;
 import com.rabbitminers.extendedgears.cogwheels.CustomCogwheelTileEntity;
 import com.rabbitminers.extendedgears.cogwheels.CustomCogwheelTileInstance;
 import com.rabbitminers.extendedgears.cogwheels.CustomCogwheelTileRenderer;
+import com.rabbitminers.extendedgears.cogwheels.ShaftlessCogwheelTileEntity;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
 
@@ -17,6 +18,13 @@ public class ExtendedCogwheelsTileEntities {
                     .validBlocks(ExtendedCogwheelsBlocks.LARGE_METAL_COGWHEELS.toArray())
                     .validBlocks(ExtendedCogwheelsBlocks.WOODEN_COGWHEELS.toArray())
                     .validBlocks(ExtendedCogwheelsBlocks.LARGE_WOODEN_COGWHEELS.toArray())
+                    .renderer(() -> CustomCogwheelTileRenderer::new)
+                    .register();
+
+    public static final BlockEntityEntry<ShaftlessCogwheelTileEntity> SHAFTLESS_COGWHEEL_TILE_ENTITY =
+            REGISTRATE.tileEntity("shaftlesscogwheeltileentity", ShaftlessCogwheelTileEntity::new)
+                    .instance(() -> CustomCogwheelTileInstance::new, false)
+                    .validBlocks(ExtendedCogwheelsBlocks.SHAFTLESS_WOODEN_COGWHEELS.toArray())
                     .renderer(() -> CustomCogwheelTileRenderer::new)
                     .register();
 
