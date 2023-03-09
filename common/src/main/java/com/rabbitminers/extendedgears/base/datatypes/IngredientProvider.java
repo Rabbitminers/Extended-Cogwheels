040f824de37bef9eafa18fcd8826087a5c8ade9e
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-09 22:18:12 +0000

    Converted IngredientProvider to record

--- common/src/main/java/com/rabbitminers/extendedgears/base/datatypes/IngredientProvider.java
+++ common/src/main/java/com/rabbitminers/extendedgears/base/datatypes/IngredientProvider.java
@@ -4,18 +4,11 @@ import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.Locale;
 
-public class IngredientProvider {
-    public final Namespace namespace;
-    public final Ingredient ingredient;
-
-    public IngredientProvider(Namespace namespace, Ingredient ingredient) {
-        this.namespace = namespace;
-        this.ingredient = ingredient;
-    }
+public record IngredientProvider(com.rabbitminers.extendedgears.base.datatypes.IngredientProvider.Namespace namespace,
+                                 Ingredient ingredient) {
 
     public enum Namespace {
-        FORGE, FABRIC, COMMON
-        ;
+        FORGE, FABRIC, COMMON;
 
         public String asId() {
             return name().toLowerCase(Locale.ROOT);
