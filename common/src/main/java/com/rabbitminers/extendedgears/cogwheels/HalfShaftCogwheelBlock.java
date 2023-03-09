5926e8b899eb07f5557f12c1190759d2e89f4fbe
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-09 18:12:43 +0000

    Added Directional Half Shaft Voxel Shape

--- common/src/main/java/com/rabbitminers/extendedgears/cogwheels/HalfShaftCogwheelBlock.java
+++ common/src/main/java/com/rabbitminers/extendedgears/cogwheels/HalfShaftCogwheelBlock.java
@@ -3,19 +3,30 @@ package com.rabbitminers.extendedgears.cogwheels;
 import com.jozufozu.flywheel.core.PartialModel;
 import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsTileEntities;
 import com.simibubi.create.AllBlockPartials;
+import com.simibubi.create.AllShapes;
 import com.simibubi.create.content.contraptions.base.KineticTileEntity;
+import com.simibubi.create.foundation.utility.VoxelShaper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class HalfShaftCogwheelBlock extends CustomCogwheelBlock {
+    public VoxelShaper voxelShape = shapeBuilder(box(2.0D, 6.0D, 2.0D, 14.0D, 10.0D, 14.0D))
+            .add(6.0D, 8, 6.0D, 10.0D, 16, 10.0D).forDirectional();
+    public VoxelShaper largeVoxelShape = shapeBuilder(box(0.0D, 6.0D, 0.0D, 16.0D, 10.0D, 16.0D))
+            .add(6.0D, 8, 6.0D, 10.0D, 16, 10.0D).forDirectional();
+
     public static final BooleanProperty AXIS_DIRECTION = BooleanProperty.create("axis_direction");
 
     public HalfShaftCogwheelBlock(boolean large, Properties properties, PartialModel model) {
@@ -40,6 +51,17 @@ public class HalfShaftCogwheelBlock extends CustomCogwheelBlock {
     }
 
     @Override
+    public @NotNull VoxelShape getShape(BlockState state, @NotNull BlockGetter worldIn, @NotNull BlockPos pos,
+                                        @NotNull CollisionContext context) {
+        if (!state.hasProperty(AXIS))
+            return super.getShape(state, worldIn, pos, context);
+        Direction dir = Direction.fromAxisAndDirection(state.getValue(AXIS),
+                directionFromValue(state.getValue(AXIS_DIRECTION)));
+        return isLargeCog() ? largeVoxelShape.get(dir) : voxelShape.get(dir);
+    }
+
+
+    @Override
     public BlockEntityType<? extends KineticTileEntity> getTileEntityType() {
         return ExtendedCogwheelsTileEntities.CUSTOM_COGWHEEL_TILE_ENTITY.get();
     }
@@ -50,6 +72,10 @@ public class HalfShaftCogwheelBlock extends CustomCogwheelBlock {
 
     public static Direction.AxisDirection directionFromValue(boolean bool) {
         return bool ? Direction.AxisDirection.POSITIVE : Direction.AxisDirection.NEGATIVE;
+    }
+
+    public static AllShapes.Builder shapeBuilder(VoxelShape shape) {
+        return new AllShapes.Builder(shape);
     }
 
     @Override
