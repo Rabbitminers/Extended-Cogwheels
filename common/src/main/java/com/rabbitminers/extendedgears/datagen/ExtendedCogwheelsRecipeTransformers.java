01050e45c9b82f4d6524ec4a98bdb237fca90d50
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-11 16:49:20 +0000

    Added Recipe Transformer Interface

--- common/src/main/java/com/rabbitminers/extendedgears/datagen/ExtendedCogwheelsRecipeTransformers.java
+++ common/src/main/java/com/rabbitminers/extendedgears/datagen/ExtendedCogwheelsRecipeTransformers.java
@@ -2,8 +2,13 @@ package com.rabbitminers.extendedgears.datagen;
 
 import com.rabbitminers.extendedgears.base.data.ICogwheelMaterial;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import org.apache.commons.lang3.function.TriFunction;
 
 public class ExtendedCogwheelsRecipeTransformers {
+    public interface RecipeTransformer extends TriFunction<ShapelessRecipeBuilder, ICogwheelMaterial, Boolean, ShapelessRecipeBuilder> {
+
+    }
+
     public static ShapelessRecipeBuilder standardCogwheelTransformer(ShapelessRecipeBuilder builder, ICogwheelMaterial material,
                                                                boolean isLarge) {
         return builder.requires(ExtendedCogwheelsRecipeProvider.I.shaft()).requires(material.getIngredient().ingredient(), isLarge ? 2 : 1);
