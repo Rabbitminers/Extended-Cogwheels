3fa00a7f4ab3bf4d433e81efacb5c342518cfebc
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-11 19:14:50 +0000

    Added Deploying Recipe Transformers

--- common/src/main/java/com/rabbitminers/extendedgears/datagen/ExtendedCogwheelsRecipeTransformers.java
+++ common/src/main/java/com/rabbitminers/extendedgears/datagen/ExtendedCogwheelsRecipeTransformers.java
@@ -1,8 +1,13 @@
 package com.rabbitminers.extendedgears.datagen;
 
 import com.rabbitminers.extendedgears.base.data.ICogwheelMaterial;
+import com.simibubi.create.content.contraptions.processing.ProcessingRecipe;
+import com.simibubi.create.content.contraptions.processing.ProcessingRecipeBuilder;
+import com.tterrag.registrate.util.entry.BlockEntry;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.world.level.block.Block;
 import org.apache.commons.lang3.function.TriFunction;
+import org.jetbrains.annotations.Nullable;
 
 public class ExtendedCogwheelsRecipeTransformers {
     public interface RecipeTransformer extends TriFunction<ShapelessRecipeBuilder, ICogwheelMaterial, Boolean, ShapelessRecipeBuilder> {
@@ -24,4 +29,30 @@ public class ExtendedCogwheelsRecipeTransformers {
         return builder.requires(material.getIngredient().ingredient(), isLarge ? 2 : 1)
                 .requires(material.getSmallIngredient().ingredient());
     }
+
+    public static <T extends ProcessingRecipe<?>> ProcessingRecipeBuilder<T> standardCogwheelTransformer(ProcessingRecipeBuilder<T> builder, ICogwheelMaterial material,
+                                                                                                      BlockEntry<?> smallCogwheel, @Nullable BlockEntry<?> largeCogwheel, boolean isLarge) {
+        return builder.require(isLarge && largeCogwheel != null ? smallCogwheel.get() : ExtendedCogwheelsRecipeProvider.I.shaft())
+                .require(material.getIngredient().ingredient())
+                .output(isLarge && largeCogwheel != null ? largeCogwheel.asStack() : smallCogwheel.asStack());
+    }
+
+    public static <T extends ProcessingRecipe<?>> ProcessingRecipeBuilder<T> halfShaftCogwheelTransformer(ProcessingRecipeBuilder<T> builder, ICogwheelMaterial material,
+                                                                                                         BlockEntry<?> smallCogwheel, @Nullable BlockEntry<?> largeCogwheel, boolean isLarge) {
+        return builder.require(isLarge && largeCogwheel != null ? smallCogwheel.get() : ExtendedCogwheelsRecipeProvider.I.andesite())
+                .require(material.getIngredient().ingredient())
+                .output(isLarge && largeCogwheel != null ? largeCogwheel.asStack() : smallCogwheel.asStack());
+    }
+
+    public static <T extends ProcessingRecipe<?>> ProcessingRecipeBuilder<T> shaftlessCogwheelTransformer(ProcessingRecipeBuilder<T> builder, ICogwheelMaterial material,
+                                                                                                         BlockEntry<?> smallCogwheel, @Nullable BlockEntry<?> largeCogwheel, boolean isLarge) {
+        if (isLarge && largeCogwheel != null) {
+            return builder.require(smallCogwheel.get()).require(material.getIngredient().ingredient())
+                    .output(largeCogwheel.asStack());
+        } else {
+            return builder.require(material.getIngredient().ingredient()).require(material.getSmallIngredient()
+                    .ingredient()).output(smallCogwheel.asStack());
+        }
+    }
+
 }
