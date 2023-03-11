78199a5e9b9b0d4506b6bc8a7aeb53618f0146be
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-11 19:14:33 +0000

    Overhauled Deploying Recipe Gen

--- common/src/main/java/com/rabbitminers/extendedgears/datagen/ExtendedCogwheelsDeployingRecipeGen.java
+++ common/src/main/java/com/rabbitminers/extendedgears/datagen/ExtendedCogwheelsDeployingRecipeGen.java
@@ -1,17 +1,25 @@
 package com.rabbitminers.extendedgears.datagen;
 
+import com.mojang.datafixers.util.Function5;
 import com.rabbitminers.extendedgears.base.data.ICogwheelMaterial;
 import com.rabbitminers.extendedgears.base.data.MetalCogwheel;
 import com.rabbitminers.extendedgears.base.data.WoodenCogwheel;
+import com.rabbitminers.extendedgears.base.datatypes.CogwheelMaterialList;
 import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsBlocks;
 import com.simibubi.create.AllItems;
 import com.simibubi.create.AllRecipeTypes;
 import com.simibubi.create.AllTags;
+import com.simibubi.create.content.contraptions.processing.ProcessingRecipeBuilder;
 import com.simibubi.create.foundation.utility.recipe.IRecipeTypeInfo;
+import com.tterrag.registrate.util.entry.BlockEntry;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.world.level.block.Block;
+import org.apache.commons.lang3.function.TriFunction;
 
+import javax.annotation.Nullable;
 import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.EnumMap;
@@ -27,34 +35,61 @@ public class ExtendedCogwheelsDeployingRecipeGen extends ExtendedCogwheelsProces
         return isLarge ? "large" : "small";
     }
 
-    private <T extends Enum<T> & ICogwheelMaterial> Map<T, GeneratedRecipe> sequencedCogwheelGenerator(Class<T> materialType, boolean isLarge) {
-        return Arrays.stream(materialType.getEnumConstants())
-                .flatMap(material -> Arrays.stream(material.getIngredients())
-                        .map(provider -> new AbstractMap.SimpleEntry<>(material,
-                                create(boolToSize(isLarge) + "_" + material.asId() + "_" + provider.namespace().asId(),
-                                        b -> b.require(isLarge ? material.getLargeCogwheel().get() : I.shaft())
-                                                .require(provider.ingredient())
-                                                .output(material.getCogwheel(isLarge).asStack())))))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a, () ->
-                        new EnumMap<>(materialType)));
+    private <T extends Enum<T> & ICogwheelMaterial> GeneratedRecipe deployedCogwheel(BlockEntry<?> smallCogwheeel, T material,
+            Function5<ProcessingRecipeBuilder, T, BlockEntry<?>, BlockEntry<?>, Boolean, ProcessingRecipeBuilder> transformer) {
+        return create(material.asId() + "_cogwheel",
+                b -> transformer.apply(b, material, smallCogwheeel, null, false));
     }
 
-    private <T extends Enum<T> & ICogwheelMaterial> Map<T, GeneratedRecipe> smallSequencedCogwheelGenerator(Class<T> materialType) {
-        return sequencedCogwheelGenerator(materialType, false);
+    private <T extends Enum<T> & ICogwheelMaterial> GeneratedRecipe largeDeployedCogwheel(BlockEntry<?> smallCogwheel, BlockEntry<?> largeCogwheel,
+            T material, Function5<ProcessingRecipeBuilder, T, BlockEntry<?>, BlockEntry<?>, Boolean, ProcessingRecipeBuilder> transformer) {
+        return create(material.asId() + "_cogwheel",
+                b -> transformer.apply(b, material, smallCogwheel, largeCogwheel, true));
     }
 
-    private <T extends Enum<T> & ICogwheelMaterial> Map<T, GeneratedRecipe> largeSequencedCogwheelGenerator(Class<T> materialType) {
-        return sequencedCogwheelGenerator(materialType, true);
+    private <T extends Enum<T> & ICogwheelMaterial> Map<T, GeneratedRecipe> deployedCogwheelMapper(CogwheelMaterialList<? extends Block, T> cogwheels,
+           Class<T> materialType, Function5<ProcessingRecipeBuilder, T, BlockEntry<?>, BlockEntry<?>, Boolean, ProcessingRecipeBuilder> transformer) {
+        Map<T, GeneratedRecipe> map = new EnumMap<T, GeneratedRecipe>(materialType);
+        for (T material : materialType.getEnumConstants()) {
+            map.put(material, deployedCogwheel(cogwheels.get(material), material, transformer));
+        }
+        return map;
     }
 
-    final Map<WoodenCogwheel, GeneratedRecipe>
-            WOODEN_COGWHEELS = smallSequencedCogwheelGenerator(WoodenCogwheel.class),
-            LARGE_WOODEN_COGWHEELS = largeSequencedCogwheelGenerator(WoodenCogwheel.class);
+    private <T extends Enum<T> & ICogwheelMaterial> Map<T, GeneratedRecipe> largeDeployedCogwheelMapper(CogwheelMaterialList<? extends Block, T> smallCogwheels,
+        CogwheelMaterialList<? extends Block, T> largeCogwheels, Class<T> materialType, Function5<ProcessingRecipeBuilder, T, BlockEntry<?>, BlockEntry<?>, Boolean, ProcessingRecipeBuilder> transformer) {
+        Map<T, GeneratedRecipe> map = new EnumMap<T, GeneratedRecipe>(materialType);
+        for (T material : materialType.getEnumConstants()) {
+            map.put(material, largeDeployedCogwheel(smallCogwheels.get(material), largeCogwheels.get(material), material, transformer));
+        }
+        return map;
+    }
 
+    private <T extends Enum<T> & ICogwheelMaterial> CogwheelRecipePair<T> smallAndLargeDeployedRecipe(CogwheelMaterialList<? extends Block, T> smallCogwheels,
+           CogwheelMaterialList<? extends Block, T> largeCogwheels, Class<T> materialType,  Function5<ProcessingRecipeBuilder, T, BlockEntry<?>, BlockEntry<?>, Boolean, ProcessingRecipeBuilder> transformer) {
+        return new CogwheelRecipePair<>(deployedCogwheelMapper(smallCogwheels, materialType, transformer),
+                largeDeployedCogwheelMapper(smallCogwheels, largeCogwheels, materialType, transformer));
+    }
 
-    final Map<MetalCogwheel, GeneratedRecipe>
-            METAL_COGWHEELS = smallSequencedCogwheelGenerator(MetalCogwheel.class),
-            LARGE_METAL_COGWHEELS = largeSequencedCogwheelGenerator(MetalCogwheel.class);
+    final CogwheelRecipePair<MetalCogwheel>
+        METAL_COGWHEELS = smallAndLargeDeployedRecipe(ExtendedCogwheelsBlocks.METAL_COGWHEELS, ExtendedCogwheelsBlocks.LARGE_METAL_COGWHEELS,
+            MetalCogwheel.class, ExtendedCogwheelsRecipeTransformers::standardCogwheelTransformer),
+        HALF_SHAFT_METAL_COGWHEELS = smallAndLargeDeployedRecipe(ExtendedCogwheelsBlocks.HALF_SHAFT_METAL_COGWHEELS, ExtendedCogwheelsBlocks.LARGE_HALF_SHAFT_METAL_COGWHEELS,
+            MetalCogwheel.class, ExtendedCogwheelsRecipeTransformers::halfShaftCogwheelTransformer),
+        SHAFTLESS_METAL_COGWHEELS = smallAndLargeDeployedRecipe(ExtendedCogwheelsBlocks.SHAFTLESS_METAL_COGWHEELS, ExtendedCogwheelsBlocks.LARGE_SHAFTLESS_METAL_COGWHEELS,
+            MetalCogwheel.class, ExtendedCogwheelsRecipeTransformers::shaftlessCogwheelTransformer);
+
+    final CogwheelRecipePair<WoodenCogwheel>
+        WOODEN_COGWHEELS = smallAndLargeDeployedRecipe(ExtendedCogwheelsBlocks.WOODEN_COGWHEELS, ExtendedCogwheelsBlocks.LARGE_WOODEN_COGWHEELS,
+            WoodenCogwheel.class, ExtendedCogwheelsRecipeTransformers::standardCogwheelTransformer),
+        HALF_SHAFT_WOODEN_COGWHEELS = smallAndLargeDeployedRecipe(ExtendedCogwheelsBlocks.HALF_SHAFT_WOODEN_COGWHEELS, ExtendedCogwheelsBlocks.LARGE_HALF_SHAFT_WOODEN_COGWHEELS,
+            WoodenCogwheel.class, ExtendedCogwheelsRecipeTransformers::halfShaftCogwheelTransformer),
+        SHAFTLESS_WOODEN_COGWHEELS = smallAndLargeDeployedRecipe(ExtendedCogwheelsBlocks.SHAFTLESS_WOODEN_COGWHEELS, ExtendedCogwheelsBlocks.LARGE_SHAFTLESS_WOODEN_COGWHEELS,
+            WoodenCogwheel.class, ExtendedCogwheelsRecipeTransformers::shaftlessCogwheelTransformer);
+
+    private record CogwheelRecipePair<T extends Enum<T> & ICogwheelMaterial>(Map<T, GeneratedRecipe> small, Map<T, GeneratedRecipe> large) {
+
+    }
 
     @ExpectPlatform
     public static RecipeProvider create(DataGenerator gen) {
