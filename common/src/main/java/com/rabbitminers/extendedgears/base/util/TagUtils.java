5f4b19c8d2f9791ceedb43e318de63cb43f7da6c
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-11 16:18:37 +0000

    Created Tag Utils

---
+++ common/src/main/java/com/rabbitminers/extendedgears/base/util/TagUtils.java
@@ -0,0 +1,20 @@
+package com.rabbitminers.extendedgears.base.util;
+
+import com.simibubi.create.AllTags;
+import dev.architectury.injectables.annotations.ExpectPlatform;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class TagUtils {
+    @ExpectPlatform
+    public static TagKey<Item> steelIngot() {
+        throw new AssertionError();
+    }
+
+    @ExpectPlatform
+    public static TagKey<Item> steelNugget() {
+        throw new AssertionError();
+    }
+}
