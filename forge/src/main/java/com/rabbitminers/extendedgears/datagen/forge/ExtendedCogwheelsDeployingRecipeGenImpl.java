071dee2db2cab29269f8cce6eb18e10cb946fe48
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-12 01:04:01 +0000

    Implemented Deploying Conditions To Forge

--- forge/src/main/java/com/rabbitminers/extendedgears/datagen/forge/ExtendedCogwheelsDeployingRecipeGenImpl.java
+++ forge/src/main/java/com/rabbitminers/extendedgears/datagen/forge/ExtendedCogwheelsDeployingRecipeGenImpl.java
@@ -3,12 +3,23 @@ package com.rabbitminers.extendedgears.datagen.forge;
 import com.rabbitminers.extendedgears.datagen.ExtendedCogwheelsDeployingRecipeGen;
 import com.rabbitminers.extendedgears.datagen.ExtendedCogwheelsProcessingRecipeGen;
 import com.simibubi.create.AllRecipeTypes;
+import com.simibubi.create.content.contraptions.processing.ProcessingRecipe;
+import com.simibubi.create.content.contraptions.processing.ProcessingRecipeBuilder;
 import com.simibubi.create.foundation.utility.recipe.IRecipeTypeInfo;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.crafting.conditions.AndCondition;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import net.minecraftforge.common.crafting.conditions.NotCondition;
+import net.minecraftforge.common.crafting.conditions.TagEmptyCondition;
 import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class ExtendedCogwheelsDeployingRecipeGenImpl extends ExtendedCogwheelsProcessingRecipeGen {
@@ -29,5 +40,16 @@ public class ExtendedCogwheelsDeployingRecipeGenImpl extends ExtendedCogwheelsPr
     @Override
     protected IRecipeTypeInfo getRecipeType() {
         return AllRecipeTypes.DEPLOYING;
+    }
+
+    @SafeVarargs
+    public static <T extends ProcessingRecipe<?>> ProcessingRecipeBuilder<T> whenTagsPopulated(ProcessingRecipeBuilder<T> builder,
+                                                                                               @Nullable TagKey<Item>... tagKeys) {
+        if (tagKeys == null) return builder;
+        ICondition[] conditions = Arrays.stream(tagKeys)
+                .filter(Objects::nonNull)
+                .map(tagKey -> new NotCondition(new TagEmptyCondition(tagKey.location())))
+                .toArray(ICondition[]::new);
+        return builder.withCondition(new AndCondition(conditions));
     }
 }
