239ad67b4526347ffe359fec99d4b4d6d29fc036
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-13 17:14:47 +0000

    Implemented Cutting To Forge

---
+++ forge/src/main/java/com/rabbitminers/extendedgears/datagen/forge/ExtendedCogwheelsCuttingRecipeGenImpl.java
@@ -0,0 +1,34 @@
+package com.rabbitminers.extendedgears.datagen.forge;
+
+import com.rabbitminers.extendedgears.datagen.ExtendedCogwheelsCuttingRecipeGen;
+import com.rabbitminers.extendedgears.datagen.ExtendedCogwheelsDeployingRecipeGen;
+import com.rabbitminers.extendedgears.datagen.ExtendedCogwheelsProcessingRecipeGen;
+import com.simibubi.create.AllRecipeTypes;
+import com.simibubi.create.foundation.utility.recipe.IRecipeTypeInfo;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeProvider;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+
+public class ExtendedCogwheelsCuttingRecipeGenImpl extends ExtendedCogwheelsProcessingRecipeGen {
+    public ExtendedCogwheelsCuttingRecipeGenImpl(DataGenerator generator) {
+        super(generator);
+    }
+
+    public static RecipeProvider create(DataGenerator gen) {
+        ExtendedCogwheelsCuttingRecipeGen provider = new ExtendedCogwheelsCuttingRecipeGen(gen);
+        return new RecipeProvider(gen) {
+            @Override
+            protected void buildCraftingRecipes(@NotNull Consumer<FinishedRecipe> consumer) {
+                provider.registerRecipes(consumer);
+            }
+        };
+    }
+
+    @Override
+    protected IRecipeTypeInfo getRecipeType() {
+        return AllRecipeTypes.CUTTING;
+    }
+}
