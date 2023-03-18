0f2293622ca7055c80cb991df37210984cf7bc44
Charlie Wood <rabbitminers2.0@gmail.com>
2023-03-18 14:24:53 +0000

    Disabled Partials Loading

--- common/src/main/java/com/rabbitminers/extendedgears/ExtendedCogwheelsClient.java
+++ common/src/main/java/com/rabbitminers/extendedgears/ExtendedCogwheelsClient.java
@@ -5,6 +5,6 @@ import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsPartials;
 public class ExtendedCogwheelsClient {
 
     public static void init() {
-        ExtendedCogwheelsPartials.init();
+
     }
 }
