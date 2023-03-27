3dfea464adda3073bee109ae40dbfb9268c90bc0
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-03-27 17:54:48 +0100

    Added Builder & Registry To Plugin

--- common/src/main/java/com/rabbitminers/extendedgears/integration/ExtendedCogwheelKubeJsPlugin.java
+++ common/src/main/java/com/rabbitminers/extendedgears/integration/ExtendedCogwheelKubeJsPlugin.java
@@ -1,21 +1,39 @@
 package com.rabbitminers.extendedgears.integration;
 
+import com.rabbitminers.extendedgears.ExtendedCogwheels;
+import com.rabbitminers.extendedgears.integration.registry.KubeJsTileEntities;
+import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsItems;
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import dev.architectury.injectables.annotations.ExpectPlatform;
 import dev.latvian.mods.kubejs.KubeJS;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.RegistryObjectBuilderTypes;
 import dev.latvian.mods.kubejs.script.ScriptType;
 
 import java.util.ArrayList;
 
 public class ExtendedCogwheelKubeJsPlugin extends KubeJSPlugin {
+    public static boolean ran = false;
     private static final CreateRegistrate REGISTRATE = CreateRegistrate.create(KubeJS.MOD_ID)
-            .creativeModeTab(() -> KubeJS.tab);
+            .creativeModeTab(() -> ExtendedCogwheelsItems.itemGroup);
+
+    @Override
+    public void init() {
+        RegistryObjectBuilderTypes.BLOCK.addType("cogwheel", KubeJsCogwheelBuilder.class, KubeJsCogwheelBuilder::new);
+    }
 
     @Override
     public void afterInit() {
-        CogwheelMaterialRegisterEvent event = new CogwheelMaterialRegisterEvent(new ArrayList<>());
-        event.post(ScriptType.STARTUP, "create.cogwheel");
-        // TODO: REGISTER SHITE
+        this.registerRegistrateRegistration();
+    }
+
+    // init methods are called twice which would cause double registration
+    public void registerRegistrateRegistration() {
+        KubeJsTileEntities.init();
+        if (!ran) {
+            REGISTRATE.register();
+            ran = true;
+        }
     }
 
     public static CreateRegistrate registrate() {
