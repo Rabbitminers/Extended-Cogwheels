0eb3863341dd0bef4eb4adc5f812b9dd39606fbb
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-03-28 21:20:15 +0100

    Added Builders For Other Cogwheel Types

--- common/src/main/java/com/rabbitminers/extendedgears/integration/ExtendedCogwheelKubeJsPlugin.java
+++ common/src/main/java/com/rabbitminers/extendedgears/integration/ExtendedCogwheelKubeJsPlugin.java
@@ -5,12 +5,15 @@ import com.rabbitminers.extendedgears.integration.registry.KubeJsTileEntities;
 import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsItems;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import dev.latvian.mods.kubejs.BuiltinKubeJSPlugin;
 import dev.latvian.mods.kubejs.KubeJS;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.RegistryObjectBuilderTypes;
 import dev.latvian.mods.kubejs.script.ScriptType;
 
 import java.util.ArrayList;
+
+import static com.rabbitminers.extendedgears.integration.KubeJsCogwheelBuilder.CogwheelType.*;
 
 public class ExtendedCogwheelKubeJsPlugin extends KubeJSPlugin {
     public static boolean ran = false;
@@ -20,9 +23,21 @@ public class ExtendedCogwheelKubeJsPlugin extends KubeJSPlugin {
     @Override
     public void init() {
         RegistryObjectBuilderTypes.BLOCK.addType("cogwheel", KubeJsCogwheelBuilder.class,
-                (location) -> new KubeJsCogwheelBuilder(location, false));
+                (location) -> new KubeJsCogwheelBuilder(location, false, STANDARD));
         RegistryObjectBuilderTypes.BLOCK.addType("large_cogwheel", KubeJsCogwheelBuilder.class,
-                (location) -> new KubeJsCogwheelBuilder(location, true));
+                (location) -> new KubeJsCogwheelBuilder(location, true, STANDARD));
+
+
+        RegistryObjectBuilderTypes.BLOCK.addType("half_shaft_cogwheel", KubeJsCogwheelBuilder.class,
+                (location) -> new KubeJsCogwheelBuilder(location, false, HALF_SHAFT));
+        RegistryObjectBuilderTypes.BLOCK.addType("large_half_shaft_cogwheel", KubeJsCogwheelBuilder.class,
+                (location) -> new KubeJsCogwheelBuilder(location, true, HALF_SHAFT));
+
+
+        RegistryObjectBuilderTypes.BLOCK.addType("shaftless_cogwheel", KubeJsCogwheelBuilder.class,
+                (location) -> new KubeJsCogwheelBuilder(location, false, SHAFTLESS));
+        RegistryObjectBuilderTypes.BLOCK.addType("large_shaftless_cogwheel", KubeJsCogwheelBuilder.class,
+                (location) -> new KubeJsCogwheelBuilder(location, true, SHAFTLESS));
     }
 
     @Override
