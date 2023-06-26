32f2e0b8ba8be2c8bd886e0d45bd811bca541159
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-26 19:49:40 +0100

    Registered procedural partial models

--- common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsPartials.java
+++ common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsPartials.java
@@ -2,36 +2,10 @@ package com.rabbitminers.extendedgears.registry;
 
 import com.jozufozu.flywheel.core.PartialModel;
 import com.rabbitminers.extendedgears.ExtendedCogwheels;
-import com.rabbitminers.extendedgears.base.data.ICogwheelMaterial;
-import com.rabbitminers.extendedgears.base.data.MetalCogwheel;
-import com.rabbitminers.extendedgears.base.data.WoodenCogwheel;
-import com.rabbitminers.extendedgears.base.datatypes.CogwheelModel;
-
-import java.util.EnumMap;
-import java.util.Map;
-
 public class ExtendedCogwheelsPartials {
-
-    public static <T extends Enum<T> & ICogwheelMaterial> Map<T, CogwheelModel> fillCogwheels(Class<T> materialType, String type) {
-        Map<T, CogwheelModel> map = new EnumMap<>(materialType);
-        String prefix = type != null ? type + "_" : "";
-        for (T material : materialType.getEnumConstants()) {
-            map.put(material, new CogwheelModel(block(prefix + material.asId() + "_cogwheel"),
-                    block("large_" + prefix + material.asId() + "_cogwheel")));
-        }
-        return map;
-    }
-
-    public static final Map<MetalCogwheel, CogwheelModel>
-            METAL_COGWHEELS = fillCogwheels(MetalCogwheel.class, null),
-            SHAFTLESS_METAL_COGWHEELS = fillCogwheels(MetalCogwheel.class, "shaftless"),
-            HALF_SHAFT_METAL_COGWHEELS = fillCogwheels(MetalCogwheel.class, "half_shaft");
-
-    public static final Map<WoodenCogwheel, CogwheelModel>
-            WOODEN_COGWHEELS = fillCogwheels(WoodenCogwheel.class, null),
-            SHAFTLESS_WOODEN_COGWHEELS = fillCogwheels(WoodenCogwheel.class, "shaftless"),
-            HALF_SHAFT_WOODEN_COGWHEELS = fillCogwheels(WoodenCogwheel.class, "half_shaft");
-
+    public static final PartialModel
+        COGWHEEL = block("proc_cogwheel"),
+        LARGE_COGWHEEL = block("proc_large_cogwheel");
 
     private static PartialModel block(String path) {
         return new PartialModel(ExtendedCogwheels.asResource("block/" + path));
