e53c8b155685523c8cb4c6e490f9ecb24376ed0c
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-26 19:48:09 +0100

    Added model compartments to extended cogwheels clients

--- common/src/main/java/com/rabbitminers/extendedgears/ExtendedCogwheelsClient.java
+++ common/src/main/java/com/rabbitminers/extendedgears/ExtendedCogwheelsClient.java
@@ -1,10 +1,13 @@
 package com.rabbitminers.extendedgears;
 
+import com.rabbitminers.extendedgears.mixin_interface.DynamicCogwheelRenderer;
 import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsPartials;
+import com.simibubi.create.foundation.render.SuperByteBufferCache;
 
 public class ExtendedCogwheelsClient {
-
+    public static final SuperByteBufferCache BUFFER_CACHE = new SuperByteBufferCache();
     public static void init() {
-
+        BUFFER_CACHE.registerCompartment(DynamicCogwheelRenderer.COGWHEEL);
+        ExtendedCogwheelsPartials.init();
     }
 }
