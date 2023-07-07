a781ee047fee888f104177b38fbae086c3df25d9
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-07 16:55:56 +0100

    Implemented model providers

---
+++ common/src/main/java/com/rabbitminers/extendedgears/mixin_interface/ICogwheelModelProvider.java
@@ -0,0 +1,7 @@
+package com.rabbitminers.extendedgears.mixin_interface;
+
+import com.jozufozu.flywheel.core.PartialModel;
+
+public interface ICogwheelModelProvider {
+    PartialModel getTemplate(boolean large);
+}
