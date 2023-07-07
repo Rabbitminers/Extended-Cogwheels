e3d8618b2cf0983cc5166f66bbd9edb1c6a1e55e
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-07 16:55:39 +0100

    Registered Half shaft and shaftless cogwheel models

--- common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsPartials.java
+++ common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsPartials.java
@@ -5,7 +5,11 @@ import com.rabbitminers.extendedgears.ExtendedCogwheels;
 public class ExtendedCogwheelsPartials {
     public static final PartialModel
         COGWHEEL = block("proc_cogwheel"),
-        LARGE_COGWHEEL = block("proc_large_cogwheel");
+        LARGE_COGWHEEL = block("proc_large_cogwheel"),
+        HALF_SHAFT_COGWHEEL = block("proc_half_shaft_cogwheel"),
+        LARGE_HALF_SHAFT_COGWHEEL = block("proc_half_shaft_large_cogwheel"),
+        SHAFTLESS_COGWHEEL = block("proc_shaftless_cogwheel"),
+        LARGE_SHAFTLESS_COGWHEEL = block("proc_shaftless_large_cogwheel");
 
     private static PartialModel block(String path) {
         return new PartialModel(ExtendedCogwheels.asResource("block/" + path));
