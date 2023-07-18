c13321b0df2c7da92e99734fe351c7ce98035267
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-18 21:43:38 +0100

    Implemented Custom Item Predicate Wrapper

---
+++ common/src/main/java/com/rabbitminers/extendedgears/base/datatypes/ItemPredicate.java
@@ -0,0 +1,42 @@
+package com.rabbitminers.extendedgears.base.datatypes;
+
+import com.simibubi.create.foundation.data.recipe.StandardRecipeGen;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.function.Predicate;
+
+public sealed interface ItemPredicate extends Predicate<ItemStack> {
+    final class ItemWrapper implements ItemPredicate {
+        private final Item item;
+
+        public ItemWrapper(Item item) {
+            this.item = item;
+        }
+        @Override
+        public boolean test(ItemStack stack) {
+            return stack.is(item);
+        }
+    }
+
+    final class TagWrapper implements ItemPredicate {
+        private final TagKey<Item> tag;
+
+        public TagWrapper(TagKey<Item> tag) {
+            this.tag = tag;
+        }
+        @Override
+        public boolean test(ItemStack stack) {
+            return stack.is(tag);
+        }
+    }
+
+    public static ItemPredicate of(Item item) {
+        return new ItemWrapper(item);
+    }
+
+    public static ItemPredicate of(TagKey<Item> tag) {
+        return new TagWrapper(tag);
+    }
+}
