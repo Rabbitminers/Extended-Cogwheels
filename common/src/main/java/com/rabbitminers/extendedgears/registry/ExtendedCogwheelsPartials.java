880ef1a2b0cb993369f74c6492350a5985faf4f6
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-18 21:43:00 +0100

    Removed legacy partials and added chunky ones

--- common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsPartials.java
+++ common/src/main/java/com/rabbitminers/extendedgears/registry/ExtendedCogwheelsPartials.java
@@ -4,12 +4,10 @@ import com.jozufozu.flywheel.core.PartialModel;
 import com.rabbitminers.extendedgears.ExtendedCogwheels;
 public class ExtendedCogwheelsPartials {
     public static final PartialModel
-        COGWHEEL = block("cogwheel"),
-        LARGE_COGWHEEL = block("large_cogwheel"),
-        HALF_SHAFT_COGWHEEL = block("half_shaft_cogwheel"),
-        LARGE_HALF_SHAFT_COGWHEEL = block("large_half_shaft_cogwheel"),
-        SHAFTLESS_COGWHEEL = block("shaftless_cogwheel"),
-        LARGE_SHAFTLESS_COGWHEEL = block("large_shaftless_cogwheel");
+        COGWHEEL = block("shaftless_cogwheel"),
+        LARGE_COGWHEEL = block("large_shaftless_cogwheel"),
+        CHUNKY_COGWHEEL = block("chunky_cogwheel"),
+        LARGE_CHUNKY_COGWHEEL = block("large_chunky_cogwheel");
 
     private static PartialModel block(String path) {
         return new PartialModel(ExtendedCogwheels.asResource("block/" + path));
