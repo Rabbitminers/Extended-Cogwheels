33d733d88642550d22d412713ea212b5446c762e
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-19 23:52:00 +0100

    Ran datagen

--- common/src/main/java/com/rabbitminers/extendedgears/mixin/MixinCogwheelBlockItem.java
+++ common/src/main/java/com/rabbitminers/extendedgears/mixin/MixinCogwheelBlockItem.java
@@ -5,8 +5,6 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 
-import java.util.List;
-
 @Mixin(CogwheelBlockItem.class)
 public class MixinCogwheelBlockItem extends BlockItem {
     public MixinCogwheelBlockItem(Block block, Properties properties) {
