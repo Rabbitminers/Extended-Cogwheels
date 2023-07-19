2bce5efe78548f7ebf1af66d8a5714b76c846d1d
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-07-19 18:14:06 +0100

    Implemented shaftless cogwheel to cogwheel interaction

--- common/src/main/java/com/rabbitminers/extendedgears/cogwheels/ShaftlessCogwheelBlock.java
+++ common/src/main/java/com/rabbitminers/extendedgears/cogwheels/ShaftlessCogwheelBlock.java
@@ -2,23 +2,32 @@ package com.rabbitminers.extendedgears.cogwheels;
 
 import com.jozufozu.flywheel.core.PartialModel;
 import com.rabbitminers.extendedgears.mixin_interface.ICogwheelModelProvider;
+import com.rabbitminers.extendedgears.mixin_interface.IDynamicMaterialBlockEntity;
 import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsPartials;
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.content.kinetics.simpleRelays.BracketedKineticBlockEntity;
 import com.simibubi.create.content.kinetics.simpleRelays.CogWheelBlock;
+import com.simibubi.create.content.kinetics.simpleRelays.ICogWheel;
 import com.simibubi.create.foundation.utility.VoxelShaper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
+
+import static com.simibubi.create.content.kinetics.base.RotatedPillarKineticBlock.AXIS;
 
 public class ShaftlessCogwheelBlock extends CogWheelBlock implements ICogwheelModelProvider {
     public VoxelShape voxelShape = Block.box(2.0D, 6.0D, 2.0D, 14.0D, 10.0D, 14.0D);
@@ -35,11 +44,31 @@ public class ShaftlessCogwheelBlock extends CogWheelBlock implements ICogwheelMo
     public static ShaftlessCogwheelBlock large(Properties properties) {
         return new ShaftlessCogwheelBlock(true, properties);
     }
-
     @Override
-    public @NotNull InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand,
-                                          BlockHitResult ray) {
-        return super.use(state, world, pos, player, hand, ray);
+    public @NotNull InteractionResult use(BlockState state, Level level, BlockPos pos, Player player,
+              InteractionHand hand, BlockHitResult ray) {
+        if (player.isShiftKeyDown() || !player.mayBuild())
+            return InteractionResult.PASS;
+        ItemStack heldItem = player.getItemInHand(hand);
+        if (heldItem.is(AllBlocks.SHAFT.get().asItem()) && !level.isClientSide) {
+            boolean isLarge = ICogWheel.isLargeCog(state);
+            BlockState newState = isLarge ? AllBlocks.LARGE_COGWHEEL.getDefaultState()
+                    : AllBlocks.COGWHEEL.getDefaultState();
+            newState = newState.setValue(AXIS, state.getValue(AXIS));
+            BlockEntity oldBe = level.getBlockEntity(pos);
+            if (!(oldBe instanceof IDynamicMaterialBlockEntity oldDyn))
+                return InteractionResult.FAIL;
+            BlockState material = oldDyn.getMaterial();
+            level.setBlock(pos, newState, 3);
+            if (!player.isCreative())
+                heldItem.shrink(1);
+            BlockEntity be = level.getBlockEntity(pos);
+            if (!(be instanceof IDynamicMaterialBlockEntity newDyn))
+                return InteractionResult.FAIL;
+            newDyn.applyMaterial(material);
+            return InteractionResult.SUCCESS;
+        }
+        return super.use(state, level, pos, player, hand, ray);
     }
 
     @Override
