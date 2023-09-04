04d82fdb9cf5cdac7049aecd28ee92c64bd79fbf
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-09-04 12:45:10 +0100

    Implemented new Cogwheel Material Management

--- common/src/main/java/com/rabbitminers/extendedgears/cogwheels/materials/CogwheelMaterial.java
+++ common/src/main/java/com/rabbitminers/extendedgears/cogwheels/materials/CogwheelMaterial.java
@@ -4,14 +4,21 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import com.jozufozu.flywheel.core.PartialModel;
 import com.rabbitminers.extendedgears.ExtendedCogwheels;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.AllTags;
 import com.simibubi.create.foundation.config.ConfigBase;
 import com.simibubi.create.infrastructure.config.AllConfigs;
 import net.minecraft.ResourceLocationException;
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.*;
@@ -19,89 +26,52 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class CogwheelMaterial implements Predicate<ItemStack> {
-    public static ConfigBase.ConfigInt MAX_SPEED = AllConfigs.server().kinetics.maxRotationSpeed;
+    public final ResourceLocation id;
+
+    // Defaults
     public static int MAX_STRESS = Integer.MAX_VALUE;
 
-    public final NonNullList<Ingredient> items = NonNullList.create();
+    // Clientside loaders
+    public final Map<ResourceLocation, ResourceLocation> textures;
+    public final boolean chunky;
 
+    // Datapack fields
+    public NonNullList<Ingredient> items = NonNullList.create();
     public int stressLimit = MAX_STRESS;
-    public int speedLimit = MAX_SPEED.get();
+    public int speedLimit = 256;
 
-    public ResourceLocation smallModel;
-    public ResourceLocation largeModel;
-
-    public Map<ResourceLocation, ResourceLocation> textures;
+    public CogwheelMaterial(ResourceLocation id, Map<ResourceLocation, ResourceLocation> textures, boolean chunky) {
+        this.id = id;
+        this.textures = textures;
+        this.chunky = chunky;
+    }
 
     @Override
     public boolean test(ItemStack itemStack) {
         return items.stream().anyMatch(ingredient -> ingredient.test(itemStack));
     }
 
-    public static CogwheelMaterial fromJson(JsonObject object) {
-        CogwheelMaterial material = new CogwheelMaterial();
+    public void updateFromJson(JsonObject object) {
         try {
             JsonElement ingredients = object.get("items");
             if (ingredients != null && ingredients.isJsonArray()) {
                 NonNullList<Ingredient> items = ingredientsFromJson(ingredients.getAsJsonArray());
-                material.items.addAll(items);
-            }
-
-            JsonElement textures = object.get("textures");
-            if (textures != null && textures.isJsonArray()) {
-                material.textures = texturesFromJson(textures.getAsJsonArray());
-            }
-
-            JsonElement small = object.get("small_model");
-            if (textures != null && small.isJsonPrimitive()) {
-                String key = small.getAsString();
-                material.smallModel = locationFromString(key);
-            }
-
-            JsonElement large = object.get("large_model");
-            if (textures != null && large.isJsonPrimitive()) {
-                String key = large.getAsString();
-                material.largeModel = locationFromString(key);
+                this.items.addAll(items);
             }
 
             parseJsonPrimitive(object, "stress_limit", JsonPrimitive::isNumber,
-                    primitive -> material.speedLimit = primitive.getAsInt());
+                    primitive -> this.speedLimit = primitive.getAsInt());
 
             parseJsonPrimitive(object, "speed_limit", JsonPrimitive::isNumber,
-                    primitive -> material.speedLimit = primitive.getAsInt());
+                    primitive -> this.speedLimit = primitive.getAsInt());
         } catch (Exception e) {
             //
         }
-        return material;
     }
 
-    private static Map<ResourceLocation, ResourceLocation> texturesFromJson(JsonArray textures) {
-        Map<ResourceLocation, ResourceLocation> map = new HashMap<>();
-        for (JsonElement element : textures) {
-            if (!element.isJsonObject())
-                continue;
-
-            JsonObject object = element.getAsJsonObject();
-            JsonElement repElement = object.get("replacement");
-            JsonElement oldElement = object.get("old");
-
-            if (oldElement == null || !oldElement.isJsonPrimitive() || repElement == null || !repElement.isJsonPrimitive())
-                continue;
-
-            try {
-                ResourceLocation old = new ResourceLocation(oldElement.getAsString());
-                ResourceLocation rep = new ResourceLocation(repElement.getAsString());
-
-                map.put(old, rep);
-            } catch (ResourceLocationException e) {
-                //
-            }
-        }
-        return map;
-    }
-
-    private static ResourceLocation locationFromString(String primitve) {
+    private static ResourceLocation locationFromString(String primitive) {
         try {
-            return new ResourceLocation(primitve);
+            return new ResourceLocation(primitive);
         } catch (ResourceLocationException e) {
             //
         }
@@ -110,51 +80,23 @@ public class CogwheelMaterial implements Predicate<ItemStack> {
 
     private static NonNullList<Ingredient> ingredientsFromJson(JsonArray ingredientArray) {
         NonNullList<Ingredient> ingredients = NonNullList.create();
-        for (int i = 0; i < ingredientArray.size(); i++) {
-            Ingredient ingredient = Ingredient.fromJson(ingredientArray.get(i));
-            if (!ingredient.isEmpty()) {
-                ingredients.add(ingredient);
-            }
+
+        for (JsonElement je : ingredientArray) {
+            ingredients.add(Ingredient.fromJson(je));
         }
+
         return ingredients;
     }
 
     public void toBuffer(FriendlyByteBuf buffer) {
+        buffer.writeResourceLocation(id);
         buffer.writeVarInt(items.size());
         for (Ingredient item : items) {
             item.toNetwork(buffer);
         }
-        buffer.writeVarInt(textures.size());
-        for (Map.Entry<ResourceLocation, ResourceLocation> texture : textures.entrySet()) {
-            buffer.writeResourceLocation(texture.getKey());
-            buffer.writeResourceLocation(texture.getValue());
-        }
-        buffer.writeResourceLocation(smallModel);
-        buffer.writeResourceLocation(largeModel);
         buffer.writeInt(speedLimit);
         buffer.writeInt(stressLimit);
     }
-
-    public static CogwheelMaterial fromBuffer(FriendlyByteBuf buffer) {
-        CogwheelMaterial type = new CogwheelMaterial();
-        int itemSize = buffer.readVarInt();
-        for (int i = 0; i < itemSize; i++) {
-            type.items.add(Ingredient.fromNetwork(buffer));
-        }
-        int textureSize = buffer.readVarInt();
-        for (int i = 0; i < textureSize; i++) {
-            ResourceLocation old = buffer.readResourceLocation();
-            ResourceLocation replacement = buffer.readResourceLocation();
-            type.textures.put(old, replacement);
-        }
-        type.smallModel = buffer.readResourceLocation();
-        type.largeModel = buffer.readResourceLocation();
-        type.speedLimit = buffer.readInt();
-        type.stressLimit = buffer.readInt();
-
-        return type;
-    }
-
 
     // Re-used from Potato cannon projectile type
     private static void parseJsonPrimitive(JsonObject object, String key, Predicate<JsonPrimitive> predicate, Consumer<JsonPrimitive> consumer) {
@@ -166,5 +108,4 @@ public class CogwheelMaterial implements Predicate<ItemStack> {
             }
         }
     }
-
 }
--- common/src/main/java/com/rabbitminers/extendedgears/cogwheels/materials/CogwheelMaterialManager.java
+++ common/src/main/java/com/rabbitminers/extendedgears/cogwheels/materials/CogwheelMaterialManager.java
@@ -6,13 +6,17 @@ import com.rabbitminers.extendedgears.base.util.Env;
 import com.rabbitminers.extendedgears.base.networking.PlayerSelection;
 import com.rabbitminers.extendedgears.base.networking.S2CPacket;
 import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsPackets;
+import com.simibubi.create.content.equipment.potatoCannon.PotatoProjectileTypeManager;
+import com.simibubi.create.infrastructure.config.AllConfigs;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.NonNullList;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 import java.util.Map.Entry;
@@ -20,20 +24,40 @@ import java.util.Map.Entry;
 public class CogwheelMaterialManager {
     public static Map<ResourceLocation, CogwheelMaterial> MATERIALS = new HashMap<>();
     public static Map<ResourceLocation, ClientCogwheelMaterial> CLIENT_MATERIALS = new HashMap<>();
-    public static Map<Item, CogwheelMaterial> ITEM_TO_MATERIALS = new IdentityHashMap<>();
+    public static Map<Item, ResourceLocation> ITEM_TO_MATERIALS = new IdentityHashMap<>();
 
-    public static Optional<CogwheelMaterial> fromItemStack(ItemStack stack) {
-        return Optional.ofNullable(ITEM_TO_MATERIALS.get(stack.getItem()));
+    private static boolean dirty = false;
+
+    @Nullable
+    public static ResourceLocation of(ItemStack stack) {
+        if (dirty) {
+            fillItemMap();
+            dirty = false;
+        }
+        return ITEM_TO_MATERIALS.get(stack.getItem());
+    }
+    
+    public static CogwheelMaterial of(ResourceLocation location) {
+        return MATERIALS.get(location);
     }
 
-    public static void fillItemMap() {
-        for (CogwheelMaterial material : MATERIALS.values()) {
-            for (Ingredient ingredient : material.items) {
-                for (ItemStack itemStack : ingredient.getItems()) {
-                    ITEM_TO_MATERIALS.put(itemStack.getItem(), material);
-                }
-            }
-        }
+    @Nullable
+    public static ClientCogwheelMaterial clientOf(ResourceLocation location) {
+        return CLIENT_MATERIALS.get(location);
+    }
+
+    public static int getStressLimit(ResourceLocation location) {
+        CogwheelMaterial material = of(location);
+        if (material == null)
+            return Integer.MAX_VALUE;
+        return material.stressLimit;
+    }
+
+    public static int getSpeedLimit(ResourceLocation location) {
+        CogwheelMaterial material = of(location);
+        if (material == null)
+            return AllConfigs.server().kinetics.maxRotationSpeed.get();
+        return material.stressLimit;
     }
 
     public static void toBuffer(FriendlyByteBuf buffer) {
@@ -48,15 +72,30 @@ public class CogwheelMaterialManager {
         clear();
 
         int size = buffer.readVarInt();
+
         for (int i = 0; i < size; i++) {
-            MATERIALS.put(buffer.readResourceLocation(), CogwheelMaterial.fromBuffer(buffer));
+            ResourceLocation id = buffer.readResourceLocation();
+            int itemSize = buffer.readVarInt();
+            NonNullList<Ingredient> items = NonNullList.create();
+            for (int j = 0; j < itemSize; j++) {
+                items.add(Ingredient.fromNetwork(buffer));
+            }
+            int speedLimit = buffer.readInt();
+            int stressLimit = buffer.readInt();
+
+            CogwheelMaterial material = MATERIALS.get(id);
+            if (material == null)
+                continue;
+
+            material.items = items;
+            material.speedLimit = speedLimit;
+            material.stressLimit = stressLimit;
         }
 
-        fillItemMap();
+        dirty = true;
     }
 
     public static void clear() {
-        MATERIALS.clear();
         ITEM_TO_MATERIALS.clear();
     }
 
@@ -74,23 +113,31 @@ public class CogwheelMaterialManager {
         for (Entry<ResourceLocation, JsonElement> entry : map.entrySet()) {
             JsonElement element = entry.getValue();
             if (element.isJsonObject()) {
-                ResourceLocation id = entry.getKey();
+                CogwheelMaterial type = MATERIALS.get(entry.getKey());
+                if (type == null) {
+                    continue;
+                }
                 JsonObject object = element.getAsJsonObject();
-                CogwheelMaterial type = CogwheelMaterial.fromJson(object);
-                MATERIALS.put(id, type);
+                type.updateFromJson(object);
             }
         }
 
-        fillItemMap();
-        tryFillClient();
+        dirty = true;
     }
 
-    private static void tryFillClient() {
+    public static void setupClientCogwheelMaterials() {
         if (Env.CURRENT == Env.CLIENT) {
             for (Entry<ResourceLocation, CogwheelMaterial> entry : MATERIALS.entrySet()) {
                 CLIENT_MATERIALS.put(entry.getKey(), ClientCogwheelMaterial.fromMaterial(entry.getValue()));
             }
         }
+    }
+
+    public static void fillItemMap() {
+        for (CogwheelMaterial material : MATERIALS.values())
+            for (Ingredient ingredient : material.items)
+                for (ItemStack itemStack : ingredient.getItems())
+                    ITEM_TO_MATERIALS.put(itemStack.getItem(), material.id);
     }
 
     public static class SyncPacket implements S2CPacket {
