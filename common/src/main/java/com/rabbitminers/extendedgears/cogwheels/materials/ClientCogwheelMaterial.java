0a78f4f2abd870d39090227023ee32687b9a55dd
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-09-04 12:44:33 +0100

    Implemented get model utility on client materials

--- common/src/main/java/com/rabbitminers/extendedgears/cogwheels/materials/ClientCogwheelMaterial.java
+++ common/src/main/java/com/rabbitminers/extendedgears/cogwheels/materials/ClientCogwheelMaterial.java
@@ -2,8 +2,7 @@ package com.rabbitminers.extendedgears.cogwheels.materials;
 
 import com.jozufozu.flywheel.core.PartialModel;
 import com.jozufozu.flywheel.core.StitchedSprite;
-import com.rabbitminers.extendedgears.cogwheels.CogwheelMaterials;
-import com.rabbitminers.extendedgears.mixin_interface.PartialModelStore;
+import com.rabbitminers.extendedgears.registry.ExtendedCogwheelsPartials;
 import com.simibubi.create.AllPartialModels;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -17,11 +16,16 @@ import java.util.Map.Entry;
 public record ClientCogwheelMaterial(Map<StitchedSprite, StitchedSprite> textures, PartialModel smallModel, PartialModel largeModel) {
     public static ClientCogwheelMaterial fromMaterial(CogwheelMaterial material) {
         Map<StitchedSprite, StitchedSprite> textures = new HashMap<>();
-        for (Entry<ResourceLocation, ResourceLocation> entry : material.textures.entrySet()) {
+        for (Entry<ResourceLocation, ResourceLocation> entry : material.textures.entrySet())
             textures.put(new StitchedSprite(entry.getKey()), new StitchedSprite(entry.getValue()));
-        }
-        PartialModel smallModel = PartialModelStore.getOrDefault(material.smallModel, AllPartialModels.SHAFTLESS_COGWHEEL);
-        PartialModel largeModel = PartialModelStore.getOrDefault(material.largeModel, AllPartialModels.SHAFTLESS_LARGE_COGWHEEL);
+        PartialModel smallModel = material.chunky ? ExtendedCogwheelsPartials.CHUNKY_COGWHEEL :
+                AllPartialModels.SHAFTLESS_COGWHEEL;
+        PartialModel largeModel = material.chunky ? ExtendedCogwheelsPartials.LARGE_CHUNKY_COGWHEEL :
+                AllPartialModels.SHAFTLESS_LARGE_COGWHEEL;
         return new ClientCogwheelMaterial(textures, smallModel, largeModel);
+    }
+
+    public PartialModel getModel(boolean large) {
+        return large ? largeModel : smallModel;
     }
 }
