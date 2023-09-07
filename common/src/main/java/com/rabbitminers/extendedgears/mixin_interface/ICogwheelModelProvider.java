cad39edf8e88f4e0707194441f966f4dc110acd6
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-09-07 15:57:03 +0100

    Removed unused mixins

--- common/src/main/java/com/rabbitminers/extendedgears/mixin_interface/ICogwheelModelProvider.java
+++
@@ -1,7 +0,0 @@
-package com.rabbitminers.extendedgears.mixin_interface;
-
-import com.jozufozu.flywheel.core.PartialModel;
-
-public interface ICogwheelModelProvider {
-    PartialModel getTemplate(boolean large);
-}
